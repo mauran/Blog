d3f60391c7d4988b1ddaa2b94a4216e5cddfb86e
Mauran Muthiah <mamu@nodes.dk>
2017-11-05 12:58:52 +0100

    Initial commit
    Signed-off-by: Mauran Muthiah <mamu@nodes.dk>

---
+++ Package.swift
@@ -0,0 +1,26 @@
+import PackageDescription
+
+let package = Package(
+    name: "blog",
+    targets: [
+        Target(name: "App"),
+        Target(name: "Run", dependencies: ["App"]),
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/redis-provider.git", majorVersion: 2),
+        .Package(url: "https://github.com/nodes-vapor/bugsnag.git", majorVersion: 1),
+        .Package(url: "https://github.com/nodes-vapor/sugar.git", majorVersion: 2),
+        .Package(url: "https://github.com/nodes-vapor/meta.git", majorVersion: 2)
+    ],
+    exclude: [
+        "Config",
+        "Database",
+        "Localization",
+        "Public",
+        "Resources",
+    ]
+)
+
