162247c404175642465b6504ea2a552549576fcb
Mauran Muthiah <mamu@nodes.dk>
2017-11-05 13:26:57 +0100

    Added samle controllers
    Signed-off-by: Mauran Muthiah <mamu@nodes.dk>

---
+++ Sources/App/Controllers/Frontend/FrontendController.swift
@@ -0,0 +1,20 @@
+import Vapor
+
+// sourcery: controller
+// sourcery: collection = Frontend
+
+internal final class FrontendController {
+    
+    private let renderer: ViewRenderer
+    
+    internal init(renderer: ViewRenderer) {
+        self.renderer = renderer
+    }
+    
+    // sourcery: route, method = get, path = /
+    internal func index(_ req: Request) throws -> ResponseRepresentable {
+        
+        return "index"
+    }
+    
+}
